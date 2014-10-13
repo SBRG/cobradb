e322c47abb0b09e3569e71c917ed64a3454f6b6c
Steve Federowicz <sfederow@ucsd.edu>
2014-10-13 01:47:05 -0700

    configuring the setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='OME Framework',
+    version='0.1dev',
+    packages=['ome',],
+    package_data={'ome': ['data/*']},
+    install_requires = ['sqlalchemy','psycopg2','numpy','scipy','cobra','pymongo','simplejson'],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
