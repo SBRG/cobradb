543fd024b868a1b0d4227eea84f5a294deaf3247
Steve Federowicz <sfederow@ucsd.edu>
2014-10-14 23:51:45 -0700

    better setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,81 @@
-from distutils.core import setup
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
-    name='OME Framework',
-    version='0.1dev',
+    name='ome',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.0.1',
+
+    description='The OME Framework',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/SBRG/ome',
+
+    # Author details
+    author='Steve Federowicz, Ali Ebrahim, Zak King, Justin Lu, Joshua Lerman, Edward OBrien',
+    author_email='sfederow@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Systems Biology :: Data Analysis',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='systems biology',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
     packages=['ome',],
+
+    # List run-time dependencies here.  These will be installed by pip when your
+    # project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/technical.html#install-requires-vs-requirements-files
+    install_requires=['sqlalchemy','numpy','scipy','simplejson','pandas','cobra','pysam'],
+
+    # List additional groups of dependencies here (e.g. development dependencies).
+    # You can install these using the following syntax, for example:
+    # $ pip install -e .[dev,test]
+    extras_require = {
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.  If using Python 2.6 or less, then these
+    # have to be included in MANIFEST.in as well.
     package_data={'ome': ['data/*']},
-    install_requires = ['sqlalchemy','psycopg2','numpy','scipy','cobra','pymongo','simplejson','pysam'],
-    license='MIT License',
-    long_description=open('README.md').read(),
+
+
 )
