44a341db1c0c4f04ad68d31876a3a63ad1d4020d
Ali Ebrahim <aebrahim@ucsd.edu>
2015-07-31 18:23:41 -0700

    model dumping sorts by bigg id
    This will make model dumping more deterministic

--- ome/dumping/model_dumping/dump.py
+++ ome/dumping/model_dumping/dump.py
@@ -35,15 +35,30 @@ def dump_model(bigg_id):
     # COBRApy uses the description as the ID sometimes. See https://github.com/opencobra/cobrapy/pull/152
     model.description = str(bigg_id)
 
+    # genes
+    logging.debug('Dumping genes')
+    gene_names = (session
+                  .query(Gene.bigg_id, Gene.name)
+                  .join(ModelGene)
+                  .filter(ModelGene.model_id == model_db.id)
+                  .order_by(Gene.bigg_id)
+                  )
+
+    for gene_id, gene_name in gene_names:
+        gene = cobra.core.Gene(gene_id)
+        gene.name = str(gene_name)
+        model.genes.append(gene)
+
     # reactions
     logging.debug('Dumping reactions')
     reactions_db = (session
                     .query(Reaction, ModelReaction, Synonym)
-                    .join(ModelReaction, ModelReaction.reaction_id == Reaction.id)
+                    .join(ModelReaction)
                     .join(OldIDSynonym, OldIDSynonym.ome_id == ModelReaction.id)
                     .join(Synonym, Synonym.id == OldIDSynonym.synonym_id)
                     .filter(ModelReaction.model_id == model_db.id)
-                    .all())
+                    .order_by(Reaction.bigg_id)
+                    )
 
     # make dictionaries and cast results
     result_dicts = []
@@ -88,29 +103,31 @@ def dump_model(bigg_id):
         r.objective_coefficient = result_dict['objective_coefficient']
         r.notes = {'original_bigg_id': result_dict['original_bigg_id']}
         reactions.append(r)
-    model.add_reactions(reactions) 
+    model.add_reactions(reactions)
 
     # metabolites
     logging.debug('Dumping metabolites')
-    metabolites_db = (session
-                      .query(Component.bigg_id, Compartment.bigg_id)
-                      .join(CompartmentalizedComponent,
-                            Component.id == CompartmentalizedComponent.component_id)
-                      .join(Compartment,
-                            Compartment.id == CompartmentalizedComponent.compartment_id)
-                      .join(ModelCompartmentalizedComponent,
-                            CompartmentalizedComponent.id == ModelCompartmentalizedComponent.compartmentalized_component_id)
-                      .filter(ModelCompartmentalizedComponent.model_id == model_db.id)
-                      .filter(Component.type == 'metabolite')
-                      .all())
+    metabolites_db = \
+        (session
+         .query(Component.bigg_id, Compartment.bigg_id, Component.name)
+         .join(CompartmentalizedComponent)
+         .join(Compartment)
+         .join(ModelCompartmentalizedComponent)
+         .filter(ModelCompartmentalizedComponent.model_id == model_db.id)
+         .filter(Component.type == 'metabolite')
+         .order_by(Component.bigg_id)
+         )
     metabolites = []
     compartments = set()
-    for component_id, compartment_id in metabolites_db:
+    for component_id, compartment_id, component_name in metabolites_db:
         if component_id is not None and compartment_id is not None:
-            m = cobra.core.Metabolite(id=str(component_id + '_' + compartment_id), compartment=str(compartment_id))
+            m = cobra.core.Metabolite(
+                id=str(component_id + '_' + compartment_id),
+                compartment=str(compartment_id))
+            m.name = component_name
             compartments.add(str(compartment_id))
             metabolites.append(m)
-    model.add_metabolites(metabolites) 
+    model.add_metabolites(metabolites)
 
     # compartments
     model.compartments = {i.bigg_id: i.name
@@ -123,21 +140,16 @@ def dump_model(bigg_id):
                  .query(ReactionMatrix.stoichiometry, Reaction.bigg_id,
                         Component.bigg_id, Compartment.bigg_id)
                  # component, compartment
-                 .join(CompartmentalizedComponent,
-                       ReactionMatrix.compartmentalized_component_id == CompartmentalizedComponent.id)
-                 .join(Component,
-                       CompartmentalizedComponent.component_id == Component.id)
-                 .join(Compartment,
-                       CompartmentalizedComponent.compartment_id == Compartment.id)
+                 .join(CompartmentalizedComponent)
+                 .join(Component)
+                 .join(Compartment)
                  # reaction
-                 .join(Reaction,
-                       ReactionMatrix.reaction_id == Reaction.id)
-                 .join(ModelReaction,
-                       Reaction.id == ModelReaction.reaction_id)
+                 .join(Reaction)
+                 .join(ModelReaction)
                  .filter(ModelReaction.model_id == model_db.id)
                  .filter(Component.type == 'metabolite')
-                 .distinct() # make sure we don't duplicate
-                 .all())
+                 .distinct()  # make sure we don't duplicate
+                 )
 
     for stoich, reaction_id, component_id, compartment_id in matrix_db:
         try:
@@ -150,7 +162,7 @@ def dump_model(bigg_id):
             # check again that we don't duplicate
             r = model.reactions.get_by_id(reaction_id)
             if m not in r.metabolites:
-                r.add_metabolites({ m: float(stoich) }) 
+                r.add_metabolites({m: float(stoich)})
         else:
             # try incremented ids
             while True:
@@ -159,26 +171,9 @@ def dump_model(bigg_id):
                     # check again that we don't duplicate
                     r = model.reactions.get_by_id(reaction_id)
                     if m not in r.metabolites:
-                        r.add_metabolites({ m: float(stoich) }) 
+                        r.add_metabolites({m: float(stoich)})
                 except KeyError:
                     break
-
-    # dump the gene names
-    gene_names = (session
-                .query(Gene.bigg_id, Gene.name)
-                .join(ModelGene)
-                .filter(ModelGene.model_id == model_db.id)
-                .all())
-    
-    for gene_id, gene_name in gene_names:
-        try:
-            model.genes.get_by_id(gene_id).name = str(gene_name)
-        except:
-            try:
-                model.genes.get_by_id(gene_name).name = str(gene_id)
-            except:
-                logging.warn('Gene not found %s with name %s' %
-                            (gene_id, gene_name))
 
     session.commit()
     session.close()
