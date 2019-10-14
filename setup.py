1d47d736bb3e9ee5b11d4dbe3f219b655c9b16b0
Zak King <zaking17@gmail.com>
2019-10-14 11:03:25 -0700

    simplify setup.py

--- setup.py
+++ setup.py
@@ -6,19 +6,12 @@ from sys import path
 # To temporarily modify sys.path
 SETUP_DIR = abspath(dirname(__file__))
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    path.insert(0, SETUP_DIR)
-    import ez_setup
-    path.pop(0)
-    ez_setup.use_setuptools()
-    from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
 
 setup(
     name='cobradb',
-    version='0.2.0',
+    version='0.3.0',
     description="""COBRAdb loads genome-scale metabolic models and genome
                    annotations into a relational database.""",
     url='https://github.com/SBRG/cobradb',
@@ -27,24 +20,24 @@ setup(
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords='systems biology, genome-scale model',
     packages=find_packages(),
-    install_requires=['SQLAlchemy>=1.0.12',
-                      'cobra==0.13.0',
-                      'python-libsbml==5.17.0',
-                      'numpy>=1.9.1',
-                      'psycopg2>=2.5.4',
-                      'biopython>=1.65',
-                      'scipy>=0.17.0',
-                      'lxml>=3.6.0',
-                      'pytest>=2.6.4',
-                      'six>=1.10.0',
-                      'tornado>=4.4.2',
-                      'escher>=1.5.0,<2',
-                      'configparser>=3.5.0'],
+    install_requires=[
+        'SQLAlchemy>=1.3.10,<2',
+        'cobra>=0.16.0,<0.17',
+        'python-libsbml>=5.18.0,<6',
+        'numpy>=1.17.2,<2',
+        'psycopg2>=2.8.3,<3',
+        'biopython>=1.74,<2',
+        'scipy>=1.3.1,<2',
+        'lxml>=4.4.1,<5',
+        'pytest>=4.6.6,<5',
+        'six>=1.12.0,<2',
+        'tornado>=4.5.3,<5',
+        'escher>=1.7.3,<2',
+        'configparser>=4.0.2,<5',
+    ],
 )
